950f42efbe976930d8ecfd3c3d6e59854c5b3162
Daniel <daniel@statosolutions.com>
2025-11-13 15:59:16 -0600

    fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { BrowserRouter } from "react-router-dom"
-import { TooltipProvider } from "@/store/TooltipContext"
-import { AuthProvider } from "@/store/AuthContext"
-import { AppProvider } from "@/store/AppContext"
 import AppShell from "./AppShell"
 
 export default function App() {
-  return (
-    <BrowserRouter>
-        <TooltipProvider>
-          <AppProvider>
-            <AppShell />
-          </AppProvider>
-        </TooltipProvider>
-    </BrowserRouter>
-  )
+  return <AppShell />
 }
