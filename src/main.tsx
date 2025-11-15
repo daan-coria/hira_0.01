d86bcbcc97946ef2408389fcf955ec0b841404b9
Daniel <daniel@statosolutions.com>
2025-11-15 15:30:45 -0600

    wrapped app inside mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import { AppProvider } from "@/store/AppContext"
-import { AuthProvider } from "@/store/AuthContext"
-import { TooltipProvider } from "@/store/TooltipContext"
-import "./styles/global.pcss"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AppProvider } from "@/store/AppContext";
+import { AuthProvider } from "@/store/AuthContext";
+import { TooltipProvider } from "@/store/TooltipContext";
 
-const rootElement = document.getElementById("root")
+import { MantineProvider } from "@mantine/core";   
+import "@mantine/core/styles.css";                 
+import "@mantine/dates/styles.css";                
+
+import "./styles/global.pcss";
+
+const rootElement = document.getElementById("root");
 if (!rootElement) {
-  throw new Error("Root element not found. Make sure <div id='root'></div> exists in index.html")
+  throw new Error("Root element not found. Make sure <div id='root'></div> exists in index.html");
 }
 
 ReactDOM.createRoot(rootElement).render(
@@ -18,10 +23,14 @@ ReactDOM.createRoot(rootElement).render(
       <AuthProvider>
         <TooltipProvider>
           <AppProvider>
-            <App />
+            
+            <MantineProvider>
+              <App />
+            </MantineProvider>
+
           </AppProvider>
         </TooltipProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
