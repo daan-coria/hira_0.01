63e8f99019c9506b37fa40ae55828c44b744be61
Daniel <daniel@statosolutions.com>
2025-11-25 23:22:42 -0600

    fixed position of the AI agent

--- src/components/AIAgent.tsx
+++ src/components/AIAgent.tsx
@@ -123,7 +123,7 @@ export default function AIAgent() {
   // -------------------------------------------------------
   //
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed top-20 right-4 z-50">
 
       {/* Floating chat bubble */}
       {!aiState.isOpen && (
